7cdf65334d0b6f15169d4057e71a312ac80f09a9
Neely Mann <NMann@talentpath.com>
2020-07-01 14:01:59 -0400

    fixed logging issues

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import NotFoundPage from './components/notFoundPage';
 import NavBar from './components/navBar'
 import Home from './components/home';
 import './App.css';
-import { Redirect, Route, Switch, BrowserRouter } from "react-router-dom";
+import { Route, Switch, BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
