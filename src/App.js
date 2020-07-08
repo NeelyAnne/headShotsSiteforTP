5b5643b9d7270bb162b7bd54da0071c5c855fb3d
Neely Mann <NMann@talentpath.com>
2020-07-08 12:35:28 -0400

    fixed bugs

--- src/App.js
+++ src/App.js
@@ -1,41 +1,15 @@
 import React from 'react';
-<<<<<<< HEAD
-import CameraComponent from './components/camera';
-import NotFoundPage from './components/notFoundPage';
-import NavBar from './components/navBar'
-import Home from './components/home';
-import Instructions from './components/Instructions'
-=======
 import NotFoundPage from './components/NotFoundPage';
 import CameraComponent from './components/CameraComponent';
 import Instructions from './components/Instructions';
 
 import "./App.css";
 import { Route, Switch, BrowserRouter } from "react-router-dom";
->>>>>>> 6e043d375010235c660ce5726ff50b5fc69cd6df
 
-
-import "./App.css";
-import { Route, Switch, BrowserRouter } from "react-router-dom";
-import Footer from "./components/Footer";
 function App() {
   return (
 
     <BrowserRouter>
-<<<<<<< HEAD
-      <div className="heading">
-        <div className="decor">.</div>
-        <h1>Headshot Application</h1>
-      </div>
-      <br />
-      <br />
-      <Switch>
-        <Route path="/" exact component={CameraComponent} />
-        <Route path="/instructions" exact component={Home} />
-        <Route path="/*" component={NotFoundPage} />
-      </Switch>
-      <Footer />
-=======
       <div id="backsplash-wrapper">
         <div id="backsplash">
           <div>
@@ -48,7 +22,6 @@ function App() {
         <Route path="/" exact component={CameraComponent} />
         <Route path="/*" component={NotFoundPage} />
       </Switch>
->>>>>>> 6e043d375010235c660ce5726ff50b5fc69cd6df
       <Instructions />
     </BrowserRouter>
   );
